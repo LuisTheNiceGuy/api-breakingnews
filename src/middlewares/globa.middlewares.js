5e85f6587232916298b4ab40957be8046d6efdd4
LuisGus <luisgus16@outlook.com.br>
2023-03-05 18:06:56 -0300

    feature: try/cacth

--- src/middlewares/globa.middlewares.js
+++ src/middlewares/globa.middlewares.js
@@ -2,27 +2,37 @@ const mongoose = require("mongoose");
 const userService = require("../services/user.service");
 
 const validId = (req, res , next) =>{
-    const id = req.params.id
+    try{
+        const id = req.params.id
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({message:"Invalid ID"})
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).send({message:"Invalid ID"})
+        }
+        next();
     }
-    next();
+    catch(err){
+        res.status(500).send({message: err.message})
+    };
 };
 
 const validUser = async (req, res, next) => {
-    const id = req.params.id
+    try{
+        const id = req.params.id
 
-    const user = await userService.findByIdService(id);
+        const user = await userService.findByIdService(id);
 
-    if(!user){
-        return res.status(400).send({message:"User not found"})
+        if(!user){
+            return res.status(400).send({message:"User not found"})
+        }
+
+        req.id = id;
+        req.user = user;
+
+        next();
     }
-
-    req.id = id;
-    req.user = user;
-
-    next();
+    catch(err){
+        res.status(500).send({message: err.message})
+    };
 };
 
 module.exports = {
