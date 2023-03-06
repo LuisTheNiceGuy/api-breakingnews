d1facc35a78de9b13a919847ad947373a47b5914
LuisGus <luisgus16@outlook.com.br>
2023-03-06 16:37:44 -0300

    feature .env

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// const express = require('express');
-// const connectDatabase = require("./src/database/db")
-// const userRoute = require('./src/routes/user.route')
-
 import express from 'express';
 import connectDatabase from './src/database/db.js';
-import userRoute from './src/routes/user.route.js'
+import userRoute from './src/routes/user.route.js';
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 3000
+const port = process.env.PORT || 3000;
 const app = express();
 
 connectDatabase();
--- src/database/db.js
+++ src/database/db.js
@@ -6,7 +6,7 @@ const connectDatabase = () => {
 
     mongoose
     .connect(
-        "mongodb+srv://lugus:5G6h7j8k9l@cluster0.uqehqaq.mongodb.net/?retryWrites=true&w=majority",
+        process.env.MONGODB_URI,
         {useNewUrlParser: true, useUnifiedTopology: true}
     )
     .then(() => console.log("MongoDB Atlas connected"))
