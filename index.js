a0f75780cce6afc9429fd0c52783efd8b20c8a8a
LuisGus <luisgus16@outlook.com.br>
2023-03-07 17:21:25 -0300

    feature: create and getAll news

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 import userRoute from './src/routes/user.route.js';
 import authRoute from './src/routes/auth.route.js';
-
+import newsRoute from './src/routes/news.route.js';
 
 dotenv.config();
 
@@ -15,5 +15,6 @@ connectDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
+app.use("/news", newsRoute);
 
 app.listen(port, () => console.log(`Servidor aberto na porta ${port}`));
