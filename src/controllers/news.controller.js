3bd27dcd13c2686b633b531bc6e603d3af56677e
LuisGus <luisgus16@outlook.com.br>
2023-03-10 16:18:51 -0300

    findById News

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,6 +1,6 @@
-import {createService, findAllService, countNews} from "../services/news.service.js";
+import {createService, findAllService, countNews, topNewsService, findByIdService} from "../services/news.service.js";
 
-const create = async (req, res) =>{
+export const create = async (req, res) =>{
   try{
     const {title, text, banner} = req.body;
 
@@ -25,55 +25,110 @@ const create = async (req, res) =>{
 
 };
 
-const findAll = async (req, res) =>{
-    let {limit, offset} = req.query;
+export const findAll = async (req, res) =>{
+  try{
+      let {limit, offset} = req.query;
 
-    limit = Number(limit);
-    offset = Number(offset);
+      limit = Number(limit);
+      offset = Number(offset);
 
-    if(!limit){
-      limit = 5;
-    };
+      if(!limit){
+        limit = 5;
+      };
 
-    if(!offset){
-      offset = 0;
-    };
+      if(!offset){
+        offset = 0;
+      };
 
-    const news = await findAllService(offset, limit);
-    const total = await countNews();
-    const currentURL = req.baseUrl
+      const news = await findAllService(offset, limit);
+      const total = await countNews();
+      const currentURL = req.baseUrl
 
-    const next = offset + limit;
-    const nextURL = next < total ? `${currentURL}?limit=${limit}&offset=${next}` : null;
+      const next = offset + limit;
+      const nextURL = next < total ? `${currentURL}?limit=${limit}&offset=${next}` : null;
 
-    const previous = offset - limit < 0 ? null : offset - limit;
-    const previousURL = previous != null ? `${currentURL}?limit=${limit}&offset=${previous}` : null
+      const previous = offset - limit < 0 ? null : offset - limit;
+      const previousURL = previous != null ? `${currentURL}?limit=${limit}&offset=${previous}` : null
 
-    if(news.length === 0){
-        return res.status(400).send({
-            message: "There are no registered news"
-        });
+      if(news.length === 0){
+          return res.status(400).send({
+              message: "There are no registered news"
+          });
+      }
+
+      res.send({
+        nextURL,
+        previousURL,
+        limit,
+        offset,
+        total,
+
+        results: news.map((item) => ({
+          id: item._id,
+          title: item.title,
+          text : item.text,
+          banner: item.banner,
+          likes: item.likes,
+          comments: item.comments,
+          name: item.user.name,
+          username: item.user.username,
+          userAvatar: item.user.avatar,
+        })),
+      });
+    }
+    catch (err) {
+      res.status(500).send({message: err.message});
+    }
+};
+
+export const topNews = async (req, res) =>{
+  try{
+    const news = await topNewsService();
+
+    if(!news){
+      return res.status(400).send({
+        message: "There are no registered news"
+      });
     }
 
     res.send({
-      nextURL,
-      previousURL,
-      limit,
-      offset,
-      total,
-
-      results: news.map((item) => ({
-        id: item._id,
-        title: item.title,
-        text : item.text,
-        banner: item.banner,
-        likes: item.likes,
-        comments: item.comments,
-        name: item.user.name,
-        username: item.user.username,
-        userAvatar: item.user.avatar,
-      })),
-    });
+      news: {
+        id: news._id,
+        title: news.title,
+        text : news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        username: news.user.username,
+        userAvatar: news.user.avatar,
+      }
+    })
+  }
+  catch (err) {
+    res.status(500).send({message: err.message});
+  }
 };
 
-export {create, findAll};
+export const findById = async (req, res) => {
+  try{
+    const { id } = req.params;
+    const news = await findByIdService(id);
+    return res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text : news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        username: news.user.username,
+        userAvatar: news.user.avatar,
+      }
+    })
+  }
+  catch (err) {
+    res.status(500).send({message: err.message});
+  }
+}
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
 
-import {create, findAll} from "../controllers/news.controller.js";
+import {create, findAll, topNews, findById} from "../controllers/news.controller.js";
 import { authMiddlware } from "../middlewares/auth.middleware.js";
 
 router.post("/", authMiddlware, create);
 router.get("/", authMiddlware, findAll);
-
+router.get("/top", topNews);
+router.get("/:id", findById)
 export default router;
